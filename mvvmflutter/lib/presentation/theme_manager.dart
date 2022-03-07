b1d41091c5f7d12726d8ffa8e9df0e7c54d0756e
wladyband <tecnico100@gmail.com>
2022-03-07 17:22:48 -0300

    estruturando o appBar

--- mvvmflutter/lib/presentation/theme_manager.dart
+++ mvvmflutter/lib/presentation/theme_manager.dart
@@ -7,12 +7,24 @@ ThemeData getApplicationTheme(){
     primaryColorLight: ColorManager.primaryOpacity70,
     primaryColorDark: ColorManager.darkPrimary,
     disabledColor: ColorManager.grey1,
-    accentColor: ColorManager.grey,
+
+      splashColor: ColorManager.primaryOpacity70,
+
+      accentColor: ColorManager.grey,
 
   cardTheme: CardTheme(
     color: ColorManager.white,
     shadowColor: ColorManager.grey,
-    elevation: AppSize.s4)
+    elevation: AppSize.s4),
+
+  appBarTheme: AppBarTheme(
+    centerTitle: true,
+    color: ColorManager.primary,
+    elevation: AppSize.s4,
+    shadowColor: ColorManager.primaryOpacity70,
+    titleTextStyle: getRegularStyle(
+        color: ColorManager.white, fontSize: FontSize.s16))
+
 
   );
 }
