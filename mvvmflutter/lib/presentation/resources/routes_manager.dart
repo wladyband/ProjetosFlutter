944658da67a4aa902a4025b647f37ec9947bf102
wladyband <tecnico100@gmail.com>
2022-03-08 17:10:59 -0300

    concluindo configuração de rotas

--- mvvmflutter/lib/presentation/resources/routes_manager.dart
+++ mvvmflutter/lib/presentation/resources/routes_manager.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/material.dart';
+import '/presentation/views.dart';
+
 class Routes {
   static const String splashRoute = "/";
   static const String onBoardingRoute = "/onBoarding";
@@ -6,4 +9,39 @@ class Routes {
   static const String forgotPasswordRoute = "/forgotPassword";
   static const String mainRoute = "/main";
   static const String storeDetailsRoute = "/storeDetails";
+}
+
+class RouterGenerator {
+  static Route<dynamic> getRoute(RouteSettings routeSettings) {
+    switch (routeSettings.name) {
+      case Routes.splashRoute:
+        return MaterialPageRoute(builder: (_) => SplashView());
+      case Routes.loginRoute:
+        return MaterialPageRoute(builder: (_) => LoginView());
+      case Routes.onBoardingRoute:
+        return MaterialPageRoute(builder: (_) => OnBoardingView());
+      case Routes.registerRoute:
+        return MaterialPageRoute(builder: (_) => RegisterView());
+      case Routes.forgotPasswordRoute:
+        return MaterialPageRoute(builder: (_) => ForgotPasswordView());
+      case Routes.mainRoute:
+        return MaterialPageRoute(builder: (_) => MainView());
+      case Routes.storeDetailsRoute:
+        return MaterialPageRoute(builder: (_) => StoreDetailsView());
+      default:
+        return unDefindedRoute();
+    }
+  }
+
+static Route<dynamic> unDefindedRoute () {
+  return MaterialPageRoute(builder: (_) => Scaffold(
+    appBar: AppBar(
+      title: Text("O caminho não foi encontrado"),
+    ),
+    body: Center(
+      child: Text("O caminho não foi encontrado"),
+    ),
+  ));
+}
+
 }
---
+++ mvvmflutter/lib/presentation/views.dart
@@ -0,0 +1,8 @@
+export './forgot_password/forgot_password.dart';
+export './login/login.dart';
+export './main/main_view.dart';
+export './onboarding/onboarding.dart';
+export './register/register.dart';
+export './splash/splash.dart';
+export './store_details/store_details.dart';
+
