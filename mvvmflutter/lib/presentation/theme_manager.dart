612ac8498f765ff98a2d032b56b7b521591e63d9
wladyband <tecnico100@gmail.com>
2022-03-08 11:50:48 -0300

    adicinar estilos de texto no tema personalizado

--- mvvmflutter/lib/presentation/theme_manager.dart
+++ mvvmflutter/lib/presentation/theme_manager.dart
@@ -48,10 +48,46 @@ ThemeData getApplicationTheme(){
     subtitle1: getRegularStyle(
         color: ColorManager.grey),
     caption: getRegularStyle(
-        color: ColorManager.grey)
-    )
+        color: ColorManager.grey),
+    ),
 
+  inputDecorationTheme: InputDecorationTheme(
+    contentPadding: EdgeInsets.all(AppPadding.p8),
+    hintStyle: getRegularStyle(color: ColorManager.grey1),
+    labelStyle: getMediumStyle(color: ColorManager.darkGrey),
+    errorStyle: getRegularStyle(color: ColorManager.error),
 
+  enabledBorder: OutlineInputBorder(
+    borderSide: BorderSide(
+        color: ColorManager.grey,
+        width: AppSize.s1_5),
+    borderRadius: BorderRadius.all(Radius.circular(AppSize.s8),
+    ),
+  ),
+  focusedBorder: OutlineInputBorder(
+      borderSide: BorderSide(
+        color: ColorManager.primary,
+        width: AppSize.s1_5),
+      borderRadius: BorderRadius.all(Radius.circular(AppSize.s8),
+      ),
+  ),
 
-  );
+  errorBorder: OutlineInputBorder(
+      borderSide: BorderSide(
+          color: ColorManager.error,
+          width: AppSize.s1_5),
+      borderRadius: BorderRadius.all(Radius.circular(AppSize.s8),
+      ),
+  ),
+  
+  focusedErrorBorder: OutlineInputBorder(
+    borderSide: BorderSide(
+        color: ColorManager.error,
+        width: AppSize.s1_5),
+    borderRadius: BorderRadius.all(Radius.circular(AppSize.s8),
+    ),
+  ),
+
+  ),
+);
 }
