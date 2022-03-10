9f92077efcc1a293fb59646c96e88fda5aadd0c9
wladyband <tecnico100@gmail.com>
2022-03-10 15:24:33 -0300

    construindo tela de onBoardingView

--- mvvmflutter/lib/presentation/onboarding/onboarding.dart
+++ mvvmflutter/lib/presentation/onboarding/onboarding.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:mvvmflutter/presentation/resources/resources.dart';
 
 class OnBoardingView extends StatefulWidget {
@@ -9,13 +10,87 @@ class OnBoardingView extends StatefulWidget {
 }
 
 class _OnBoardingViewState extends State<OnBoardingView> {
+  late final List<SliderObject> _list = _getSliderObject();
+  PageController _pageController = PageController(initialPage: 0);
+
+  int _currentIndex = 0;
+
+  List<SliderObject> _getSliderObject() => [
+    SliderObject.name(AppStrings.onBoardingTitle1,
+        AppStrings.onBoardingSubTitle1, ImageAssets.onboardingLogo1),
+    SliderObject.name(AppStrings.onBoardingTitle2,
+        AppStrings.onBoardingSubTitle2, ImageAssets.onboardingLogo2),
+    SliderObject.name(AppStrings.onBoardingTitle3,
+        AppStrings.onBoardingSubTitle3, ImageAssets.onboardingLogo3),
+    SliderObject.name(AppStrings.onBoardingTitle4,
+        AppStrings.onBoardingSubTitle4, ImageAssets.onboardingLogo4),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ColorManager.white,
-      body: Center(
-        child: Text("Bem vindos ao Boarding"),
+      appBar: AppBar(
+        elevation: AppSize.s1_5,
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarColor: ColorManager.white,
+          statusBarBrightness: Brightness.dark,
+        ),
       ),
+      body: PageView.builder(
+        controller: _pageController,
+          itemCount: _list.length,
+          onPageChanged: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          itemBuilder: (context, index) {
+          
+          }),
     );
   }
+}
+
+class OnBoardingPage extends StatelessWidget {
+  SliderObject _sliderObject;
+  OnBoardingPage(this._sliderObject, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        SizedBox(height: AppSize.s40,),
+        Padding(
+          padding: const EdgeInsets.all(AppPadding.p8),
+          child: Text(
+            _sliderObject.title,
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.headline1,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(AppPadding.p8),
+          child: Text(
+            _sliderObject.subTitle,
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.subtitle1,
+          ),
+        ),
+        SizedBox(
+          height: AppSize.s60,
+        )
+      ],
+    );
+  }
+}
+
+
+class SliderObject {
+  String title;
+  String subTitle;
+  String image;
+
+  SliderObject.name(this.title, this.subTitle, this.image);
 }
--- mvvmflutter/lib/presentation/resources/strings_manager.dart
+++ mvvmflutter/lib/presentation/resources/strings_manager.dart
@@ -1,3 +1,15 @@
 class AppStrings {
   static const String noRouteFound = "O caminho não foi encontrado";
+  static const String onBoardingTitle1 = "Ver um bom curso #1";
+  static const String onBoardingTitle2 = "Ver um bom curso #2";
+  static const String onBoardingTitle3 = "Ver um bom curso #3";
+  static const String onBoardingTitle4 = "Ver um bom curso #4";
+
+
+
+  static const String onBoardingSubTitle1 = "Ver um bom curso utilzando arquitetura limpa #1";
+  static const String onBoardingSubTitle2 = "Ver um bom curso utilzando arquitetura limpa #2";
+  static const String onBoardingSubTitle3 = "Ver um bom curso utilzando arquitetura limpa #3";
+  static const String onBoardingSubTitle4 = "Ver um bom curso utilzando arquitetura limpa #4";
+
 }
