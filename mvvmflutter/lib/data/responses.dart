0cf34d12d8e08e79ede5c03be0192b7b71a8283a
wladyband <tecnico100@gmail.com>
2022-03-15 19:40:57 -0300

    adicionando json serialization

--- mvvmflutter/lib/data/responses.dart
+++ mvvmflutter/lib/data/responses.dart
@@ -1,4 +1,5 @@
 import 'package:json_annotation/json_annotation.dart';
+part 'responses.g.dart';
 
 @JsonSerializable()
 class BaseResponse {
@@ -16,6 +17,14 @@ class CustomerResponse {
   String? name;
   @JsonKey(name: "numOfNotifications")
   int? numOfNotifications;
+
+  CustomerResponse(this.id, this.name, this.numOfNotifications);
+// from json
+  factory CustomerResponse.fromJson(Map<String, dynamic> json) =>
+      _$CustomerResponseFromJson(json);
+
+// to json
+  Map<String, dynamic> toJson() => _$CustomerResponseToJson(this);
 }
 
 @JsonSerializable()
@@ -26,6 +35,14 @@ class ContactsResponse {
   String? phone;
   @JsonKey(name: "link")
   String? link;
+
+  ContactsResponse(this.email, this.phone, this.link);
+// from json
+  factory ContactsResponse.fromJson(Map<String, dynamic> json) =>
+      _$ContactsResponseFromJson(json);
+
+// to json
+  Map<String, dynamic> toJson() => _$ContactsResponseToJson(this);
 }
 
 @JsonSerializable()
@@ -34,4 +51,12 @@ class AuthenticationResponse extends BaseResponse {
   CustomerResponse? customer;
   @JsonKey(name: "contacts")
   ContactsResponse? contacts;
+
+  AuthenticationResponse(this.customer, this.contacts);
+// from json
+  factory AuthenticationResponse.fromJson(Map<String, dynamic> json) =>
+      _$AuthenticationResponseFromJson(json);
+
+// to json
+  Map<String, dynamic> toJson() => _$AuthenticationResponseToJson(this);
 }
