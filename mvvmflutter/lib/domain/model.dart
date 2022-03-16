1933e43c30dc33c2a01fed9a165c69b85b97976e
wladyband <tecnico100@gmail.com>
2022-03-15 22:37:17 -0300

    criando mais classes models

--- mvvmflutter/lib/domain/model.dart
+++ mvvmflutter/lib/domain/model.dart
@@ -3,5 +3,28 @@ class SliderObject {
   String subTitle;
   String image;
 
-  SliderObject.name(this.title, this.subTitle, this.image);
+  SliderObject(this.title, this.subTitle, this.image);
+}
+
+class Customer {
+  String id;
+  String name;
+  int numOfNotifications;
+
+  Customer(this.id, this.name, this.numOfNotifications);
+}
+
+class Contacts {
+  String email;
+  String phone;
+  String link;
+
+  Contacts(this.email, this.phone, this.link);
+}
+
+class Authentication {
+  Customer customer;
+  Contacts contacts;
+
+  Authentication(this.customer, this.contacts);
 }
