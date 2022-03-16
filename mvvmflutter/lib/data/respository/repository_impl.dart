ca1ada2e8315886d594b7e8848892314052fb23c
wladyband <tecnico100@gmail.com>
2022-03-16 13:08:19 -0300

    adicionar o factory do Dio

--- mvvmflutter/lib/data/respository/repository_impl.dart
+++ mvvmflutter/lib/data/respository/repository_impl.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:mvvmflutter/data/network/error_handler.dart';
 
 import 'package:mvvmflutter/data/network/network_info.dart';
 import 'package:mvvmflutter/data/network/network_main.dart';
@@ -15,28 +16,32 @@ class RepositoryImpl extends Repository {
 
   RepositoryImpl(this._remoteDataSource, this._networkInfo);
 
+
   @override
   Future<Either<Failure, Authentication>> login(
       LoginRequest loginRequest) async {
     if (await _networkInfo.isConnected) {
-      // its safe to call the API
-      final response = await _remoteDataSource.login(loginRequest);
+      try {
+        // its safe to call the API
+        final response = await _remoteDataSource.login(loginRequest);
 
-      if (response.status == 0) // success
-          {
-        // return data (success)
-        // return right
-        return Right(response.toDomain());
-      } else {
-        // return biz logic error
-        // return left
-        return Left(Failure(
-            409, response.message ??
-            "foi encontrado uma lógica de erro do lado da API"));
+        if (response.status == ApiInternalStatus.SUCCESS) // success
+            {
+          // return data (success)
+          // return right
+          return Right(response.toDomain());
+        } else {
+          // return biz logic error
+          // return left
+          return Left(Failure(response.status ?? ApiInternalStatus.FAILURE,
+              response.message ?? ResponseMessage.DEFAULT));
+        }
+      } catch (error) {
+        return (Left(ErrorHandler.handle(error).failure));
       }
     } else {
       // return connection error
-      return Left(Failure(501, "Por favor, verifique sua conexão à internet"));
+      return Left(DataSource.NO_INTERNET_CONNECTION.getFailure());
     }
   }
 }
